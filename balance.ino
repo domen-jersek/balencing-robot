f2bdf504458c0c11bd3c250bf10cbc41707f6076
domen-jersek <56697594+domen-jersek@users.noreply.github.com>
2020-11-20 13:52:32 +0100

    gathering gyro data

--- balance.ino
+++ balance.ino
@@ -1,6 +1,13 @@
 #include "Wire.h"
 const int MPU_ADDR = 0x68; // I2C address
 
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+char tmp_str[7]; // temporary variable used in convert function
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
 
 void setup() {
 
